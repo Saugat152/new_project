b9e4410cc18b7604fe2a4e26bf5f7db024432cce
Saugat152 <saugatkc105@gmail.com>
2019-04-16 18:34:31 +0545

    second

---
+++ class_example4.py
@@ -0,0 +1,24 @@
+#object as arguments and return values
+
+def modify(obj):
+    obj.x = 5
+    return obj
+
+class Point:
+    def __init__ (self,x,y):
+        self.x = x
+        self.y = y
+    
+    def substract(self):
+        result = self.x - self.y
+        return result
+
+p = Point(1,2)
+q = Point (3,4)
+ 
+p = modify(p)
+q = modify (q)
+
+print (p.x, p.y)
+print (q.x, q.y)
+
